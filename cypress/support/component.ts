e26c79b2d451c9d99e450579e3aaf2ea764e1e80
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-12-18 15:01:35 +0500

    chore: fix pipeline errors

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,13 +1,13 @@
 import './commands'
+import '../env-config'
 
+/// <reference types="cypress" />
+
+// commands
 import { mount } from 'cypress/react18'
 
 // styles
 import '../../src/styles/index.scss'
-
-window.__ENV__ = {
-  API_ROOT: `http://test.com/to-dos-api`,
-}
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
