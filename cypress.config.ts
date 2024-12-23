b8f75345227641c55e12b10bed1d90ce267f999f
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-12-23 16:34:07 +0500

    chore: test6 commit check(fix cypress)

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,10 +3,7 @@ import { defineConfig } from "cypress"
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   e2e: {
-    baseUrl: `http://localhost:5173`,
-  },
-  env: {
-    API_URL: `http://localhost:5005/to-dos-api`,
+    baseUrl: `http://localhost:4006`,
   },
   component: {
     devServer: {
