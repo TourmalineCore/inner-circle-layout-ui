508c5ea279a30066d0d50aa12d845a224ccc5bab
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-12-23 16:41:53 +0500

    chore: test7 commit check(fix cypress)

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "cypress"
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  e2e: {
-    baseUrl: `http://localhost:4006`,
-  },
+  // e2e: {
+  //   baseUrl: `http://localhost:4006`,
+  // },
   component: {
     devServer: {
       framework: `react`,
