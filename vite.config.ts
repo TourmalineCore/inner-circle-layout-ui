8b3c3aab338272090f0275ad4cbb141536f99eef
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-02-21 10:18:51 +0500

    chore: remove unnecessary code

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import federation from "@originjs/vite-plugin-federation"
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
-import rewriteAll from 'vite-plugin-rewrite-all'
 
 const LOCAL_ENV_PORT = 40100
 const LAYOUT_PORT = process.env.NODE_ENV === `production` ? LOCAL_ENV_PORT : 4006
@@ -16,19 +15,12 @@ export default defineConfig({
   },
   // base: `/`, // for local docker
   base: `/layout`, // for local-env
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern',
-      },
-    },
-  },
   plugins: [
     react(),
     svgr(),
     federation({
       name: "inner_circle_layout_ui", // Unique name for the application
-      filename: "layout.js",
+      filename: "inner_circle_layout_ui.js",
       exposes: {
         "./layout": "./src/App.tsx", // Exposing the sidebar module from the specified path
       },
@@ -38,14 +30,8 @@ export default defineConfig({
         "react-router-dom",
       ],
     }),
-    rewriteAll(), // rewrite paths (needs for dist/assets/style-39bed72c.css)
   ],
   build: {
     target: "esnext",
-    // sourcemap: process.env.NODE_ENV === "production" ? false : true,
-    // minify: process.env.NODE_ENV === "production" ? true : false,
-    cssCodeSplit: false,
-    outDir: 'dist',
-    assetsDir: 'assets',
   },
 })
