f6c9917f6ada9c665aa28f37bc5f0cf88ed2a795
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-10-14 14:27:42 +0500

    fix: add employees redirect

--- src/pages/profile/routes.tsx
+++ src/pages/profile/routes.tsx
@@ -1,7 +1,7 @@
 import IconProfile from '../../assets/icons/icon-profile.svg?react'
 import IconProfileActive from '../../assets/icons/icon-profile-active.svg?react'
 
-const DEFAULT_PATH = `/profile`
+const DEFAULT_PATH = `/employees/profile`
 
 export const profileSidebarRoutes = [
   {
