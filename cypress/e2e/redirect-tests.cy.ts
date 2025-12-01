6bc2f07e3ca442d27b74fee7313576809650f933
Anastasia Kovylyaeva <139118716+akovylyaeva@users.noreply.github.com>
2025-12-01 18:45:20 +0500

    test: add redirect test

---
+++ cypress/e2e/redirect-tests.cy.ts
@@ -0,0 +1,35 @@
+describe(`Redirect tests`, () => {
+  beforeEach(`Authorize and check targetEnv`, () => {
+    cy.authByApi()
+    // eslint-disable-next-line no-console
+    console.log(`TARGET_ENV:`, Cypress.env(`TARGET_ENV`))
+  })
+
+  it(`
+  GIVEN home page
+  WHEN user goes to it
+  SHOULD redirect to /employee
+  `, () => {
+    cy.visit(`/`)
+
+    cy
+      .url()
+      .should(`contain`, `/employee`)
+  })
+
+  const customIt = Cypress.env(`TARGET_ENV`) === `prod`
+    ? it
+    : it.skip
+
+  customIt(`
+  GIVEN short domain
+  WHEN the page is loaded
+  SHOULD redirect to the long domain
+  `, () => {
+    cy.visit(Cypress.env(`SOURCE_DOMAIN`))
+
+    cy
+      .url()
+      .should(`contain`, Cypress.env(`TARGET_DOMAIN`))
+  })
+})
