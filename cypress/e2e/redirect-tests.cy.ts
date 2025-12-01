6546cde6b871b5b736f2465cb10530495bcf1289
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-12-01 15:21:09 +0500

    test: add redirect test

---
+++ cypress/e2e/redirect-tests.cy.ts
@@ -0,0 +1,34 @@
+describe(`Redirect tests`, () => {
+  beforeEach(`Authorize and check targetEnv`, () => {
+    cy.authByApi()
+  })
+
+  it(`
+  GIVEN home page
+  WHEN user goes to it
+  SHOULD redirect to /employee
+  `, () => {
+    cy.visit(`/`)
+
+    cy
+      .url()
+      .should(`contain`, `/employee`)
+  })
+
+  Cypress.env('targetEnv') == 'prod'
+    ? it
+    : it.skip(`
+      GIVEN short domain
+      WHEN the page is loaded
+      SHOULD redirect to the long domain
+      `, () => {
+      const sourceDomain = Cypress.env(`SOURCE_DOMAIN`)
+      const targetDomain = Cypress.env(`TARGET_DOMAIN`)
+
+      cy.visit(sourceDomain)
+
+      cy
+        .url()
+        .should(`contain`, targetDomain)
+    })
+})
