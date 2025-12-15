6ddc13338d970c738d6c6ec3539b68b81be79af3
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-12-15 16:30:09 +0500

    feat: add time icon

--- src/pages/time/routes.tsx
+++ src/pages/time/routes.tsx
@@ -1,3 +1,5 @@
+import IconTime from '../../assets/icons/icon-time.svg?react'
+
 const DEFAULT_PATH = `/time/tracking`
 
 export const timeSidebarRoutes = [
@@ -5,5 +7,7 @@ export const timeSidebarRoutes = [
     isWindowRedirectNecessary: true,
     path: DEFAULT_PATH,
     label: `Time Tracker`,
+    icon: <IconTime />,
+    iconActive: <IconTime />,
   },
 ]
