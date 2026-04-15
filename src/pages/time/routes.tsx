64037bccdccb381942d2cf32c671c75f08b551c6
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-04-15 09:09:16 +0500

    feat: add personal report route

--- src/pages/time/routes.tsx
+++ src/pages/time/routes.tsx
@@ -1,13 +1,35 @@
 import IconTime from '../../assets/icons/icon-time.svg?react'
+import { Permission } from '../../routes/state/AccessBasedOnPemissionsState'
 
 const DEFAULT_PATH = `/time`
 
-export const timeSidebarRoutes = [
-  {
-    isWindowRedirectNecessary: true,
-    path: `${DEFAULT_PATH}/tracking`,
-    label: `Time Tracker`,
-    icon: <IconTime />,
-    iconActive: <IconTime />,
-  },
-]
+export function getRouteForTime(accessPermissions: Map<keyof typeof Permission, boolean>) {
+  const routes = []
+
+  if (accessPermissions.get(`CanManagePersonalTimeTracker`)) {
+    routes.push({
+      isWindowRedirectNecessary: true,
+      path: `${DEFAULT_PATH}/tracking`,
+      label: `Tracker`,
+    })
+  }
+
+  if (accessPermissions.get(`CanViewPersonalReport`)) {
+    routes.push({
+      isWindowRedirectNecessary: true,
+      path: `${DEFAULT_PATH}/personal-report`,
+      label: `Personal Report`,
+    })
+  }
+
+  return [
+    {
+      isWindowRedirectNecessary: true,
+      path: `${DEFAULT_PATH}`,
+      label: `Time Tracker`,
+      icon: <IconTime />,
+      iconActive: <IconTime />,
+      routes: routes,
+    },
+  ]
+}
